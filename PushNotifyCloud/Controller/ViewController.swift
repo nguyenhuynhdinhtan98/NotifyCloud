86cbfc88fd3752d6bd00b487f49166e9be3a1a35
Tân Nguyễn <nguyenhuynhdinhtan98@gmail.com>
2023-04-19 12:25:22 +0700

    push

--- PushNotifyCloud/Controller/ViewController.swift
+++ PushNotifyCloud/Controller/ViewController.swift
@@ -9,15 +9,25 @@ import UIKit
 import CloudKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var titleLbl: UILabel!
+    
+    @IBOutlet weak var contentLbl: UILabel!
+    
+    @IBOutlet weak var linkBTN: UIButton!
+    
+    var link:String?
+    
     var notes =  [Note]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         CKService.shared.subcribe()
         NotificationCenter.default.addObserver(self, selector: #selector(handlerFetching), name: Notification.Name("internalNotifycation.fetchedRecord"), object: nil)
-        getNote()
+        NotificationCenter.default.addObserver(self, selector: #selector(handleAction), name: NSNotification.Name("internalNotification.handleAction"), object: nil)
+//        getNote()
     }
     
     func getNote() {
@@ -33,17 +43,39 @@ class ViewController: UIViewController {
             self.insert(note: note)
         }
     }
-
+    
     func insert(note: Note) {
         notes.insert(note, at: 0)
         let indexPath = IndexPath(row: 0, section: 0)
         tableView.insertRows(at: [indexPath], with: .automatic)
     }
     
-    @objc
-    func handlerFetching(_ sender: Notification) {
+    @objc func handlerFetching(_ sender: Notification) {
         guard let record = sender.object as? CKRecord,   let note = Note(record: record) else { return }
-            insert(note: note)
+        
+    }
+    
+    
+    
+    @objc func handleAction(_ sender: Notification) {
+        guard let notify = sender.object as? NSDictionary else { return }
+        if let aps = notify["aps"] as? NSDictionary {
+            let alert = aps["alert"] as? [String:Any]
+            let title = alert?["title"] as? String
+            let body = alert?["body"] as? String
+            let link = alert?["link"] as? String
+            titleLbl.text = title
+            contentLbl.text = body
+            self.link = link ?? ""
+        }
+    }
+    
+
+    @IBAction func openWebsite() {
+        if let urlDecode = link {
+            guard let url = URL(string: urlDecode) else { return }
+            UIApplication.shared.open(url)
+        }
     }
 }
 
@@ -58,7 +90,7 @@ extension ViewController: UITableViewDataSource {
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return notes.count
+        return notes.count
     }
 }
 
