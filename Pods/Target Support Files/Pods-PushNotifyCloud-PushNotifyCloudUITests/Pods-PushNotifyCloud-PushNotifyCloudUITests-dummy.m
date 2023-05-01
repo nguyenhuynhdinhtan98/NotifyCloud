afcaff8b78d0cdd30258146989ec64a443ef893b
Tân Nguyễn <nguyenhuynhdinhtan98@gmail.com>
2023-05-01 14:01:05 +0700

    p

---
+++ Pods/Target Support Files/Pods-PushNotifyCloud-PushNotifyCloudUITests/Pods-PushNotifyCloud-PushNotifyCloudUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PushNotifyCloud_PushNotifyCloudUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PushNotifyCloud_PushNotifyCloudUITests
+@end
--- PushNotifyCloud/Controller/AppDelegate.swift
+++ PushNotifyCloud/Controller/AppDelegate.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+import FirebaseMessaging
+import UserNotifications
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDelegate, MessagingDelegate  {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        Messaging.messaging().delegate = self
+        configApplePush(application)
         DispatchQueue.main.async {
             application.registerForRemoteNotifications()
         }
@@ -28,11 +32,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
         let token = tokenParts.joined()
         print("Device Token: \(token)")
+        
     }
     
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
         print("failed to register for remote notifications with with error: \(error)")
     }
     
+    func configApplePush(_ application: UIApplication) {
+           if #available(iOS 10.0, *) {
+               UNUserNotificationCenter.current().delegate = self
+               let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+               UNUserNotificationCenter.current().requestAuthorization(
+                   options: authOptions,
+                   completionHandler: {_, _ in })
+           } else {
+               let settings: UIUserNotificationSettings =
+               UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+               application.registerUserNotificationSettings(settings)
+           }
+           
+           application.registerForRemoteNotifications()
+       }
+    
+    func userNotificationCenter(
+          _ center: UNUserNotificationCenter,
+          willPresent notification: UNNotification,
+          withCompletionHandler completionHandler:
+          @escaping (UNNotificationPresentationOptions) -> Void
+      ) {
+          completionHandler([[.banner, .sound]])
+      }
 }
 
